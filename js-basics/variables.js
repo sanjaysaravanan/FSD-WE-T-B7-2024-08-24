d1e4eb35eeaea7529fe95b6a9cb23f2b5adcea7c
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-08-24 12:56:56 +0530

    removed the extra //

--- js-basics/variables.js
+++ js-basics/variables.js
@@ -2,33 +2,34 @@ console.log("JS Basics Learning");
 
 // Variables
 
-/// Declaration - Type of the variable
-//// var
+// Declaration - Type of the variable
+// var
 var x;
 
-//// let - ES6 Feature
+// let - ES6 Feature
 let y;
 
-/// Initialization
+// Initialization
 x = 1000;
 
 y = 2000;
 
-//// const with initialization - ES6 Feature
+// const with initialization - ES6 Feature
 const z = 0;
 
 console.log(x, y, z);
 
-/// Re-Decalration
-//// Var
+// Re-Decalration
+// Var
 var x; // re-declaration again using var
+
 x = 900; // re-initialization
 
-//// let
-//let y; // re-declaration is not allowed
+// let
+// let y; // re-declaration is not allowed
 y = 900; // re-initialization is allowed for let
 
 console.log(x, y, z);
 
-//LHS =  RHS
+// LHS =  RHS
 var q = "Queen";
